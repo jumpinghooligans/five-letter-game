93bb1555a5d4e1cd129acedc81f12c34b431dcb0
jumpinghooligans <ryankortmann@gmail.com>
2013-10-02 21:38:09 -0400

    Added heroku DB again

---
+++ routes/dev.js
@@ -0,0 +1,7 @@
+var User = require("../models/user.js");
+var Game = require("../models/game.js");
+
+exports.inputtest = function(req, res){
+	//res.render("user/index");
+	res.render("dev/inputtest");
+};
